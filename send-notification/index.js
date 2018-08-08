6b4dfc0c1a96712bfb5952746590f686b44db608
Pedro Jesus <pjesus@wkm.com.br>
2018-08-08 11:07:20 -0300

    added notificator test

--- send-notification/index.js
+++ send-notification/index.js
@@ -1,17 +1,41 @@
-module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+var OneSignal = require('onesignal-node');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: {"name": "Olar " + (req.query.name || req.body.name)}
-        };
-    }
+module.exports = function (context, req) {
+    const oneSignalClient = new OneSignal.Client({
+        userAuthKey: "NTExYjg3YWItZTI1OS00YjFlLWFlNDYtNmQ0NDg1OTBlMzAz",
+        app: {
+            appId: "30ca6c3b-0cb3-4a92-a35e-c80d27a4f923",
+            appAuthKey: "MTFmNTU5NjgtMGQyMS00MWYyLTljZTQtYTE3N2MzMDVmM2Zk"
+        }
+    });
+
+    if (req.body && req.body.name) {
+        const notification = new OneSignal.Notification({
+            contents: {
+                en: `Notificando %{req.body.name}`;
+            }
+        });
+        oneSignalClient.sendNotification(notification, function(err, httpResponse, data) {
+            if (err) {
+                context.res = {
+                    // status: 200, /* Defaults to 200 */
+                    body: {"name": "Notificação enviada para " + req.body.name, "data": data}
+                };
+            } else {
+                context.res = {
+                    status: httpResponse.statusCode,
+                    body: {"name": "Erro ao enviar notificação para " + req.body.name, "data": data}
+                };
+            }
+
+            context.done();
+        });
+            }
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please pass a name in the request body"
         };
+        context.done();
     }
-    context.done();
 };
