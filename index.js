9b01090f2066207f5217d23f99a17b226589fb89
Pedro Jesus <pjesus@wkm.com.br>
2018-08-08 09:56:40 -0300

    changed test code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: {"name": "Hello " + (req.query.name || req.body.name)}
+            body: {"name": "Olar " + (req.query.name || req.body.name)}
         };
     }
     else {
